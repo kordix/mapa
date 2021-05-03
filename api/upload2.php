4a7cb52c65c3bb490b977162c6ee590534209520
Kordian Bober <BOBKOR@ad.bertrand.pl>
2021-05-03 13:45:42 +0200

    upload fetch

---
+++ api/upload2.php
@@ -0,0 +1,11 @@
+<?php
+ define('SITE_ROOT', realpath(dirname(__FILE__)));
+ require_once('dir.php');
+
+ $nazwapliku = $_GET['nazwapliku'];
+ $rozszerzenie = strtolower(pathinfo($_FILES["image"]["name"], PATHINFO_EXTENSION));
+
+ $target_file = $target_dir . basename($nazwapliku.'.'.$rozszerzenie);
+
+move_uploaded_file($_FILES["image"]["tmp_name"], $target_file);
+
